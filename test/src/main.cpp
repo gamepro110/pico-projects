5058c9ecd8a2b60d7756e5d26ee83e05e67a43c9
void@Debian-5950x <k.koelewijn2@gmail.com>
2023-01-15 17:50:26 +0100

    added submodule init guard + clean uptest dir

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,8 +2,12 @@
 #include "pico/stdlib.h"
 
 int main() {
-	setup_default_uart();
-	printf("Hello World");
+	stdio_init_all();
+
+    while (true) {
+        printf("Hello World");
+        sleep_ms(1000);
+    }
+
 	return 0;
 }
-
